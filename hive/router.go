bd8ebdf00e5a2111b7cced61397b08fec5b6f950
Stepan Strelets <s3lets@gmail.com>
2019-10-26 13:45:43 +0300

    Small refactoring - set tranmitter, get receiver

--- hive/router.go
+++ hive/router.go
@@ -11,19 +11,18 @@ type UserPayload struct {
 }
 
 type AppPayload struct {
-	Receiver uint32
+	Transmitter uint32
 }
 
 func RouterStart(users *Users, apps *Apps)  {
 	go func() {
 		for {
 			msg := users.ReceiveMessage()
-			var payload UserPayload
-			err := json.Unmarshal(msg.Payload, payload)
+			aid, err := extractReceiverUUID(msg)
 			if err != nil {
 				log.Error("User:%d say:%s", msg.Uid, msg.Payload)
 			} else {
-				apps.SendMessage(payload.Receiver, msg.Uid, msg.Payload)
+				apps.SendMessage(*aid, msg.Uid, msg.Payload)
 			}
 		}
 	}()
@@ -33,12 +32,46 @@ func RouterStart(users *Users, apps *Apps)  {
 			msg := apps.ReceiveMessage()
 			// send to all users connected to the app
 			if msg.Uids != nil {
-				item := msg.Uids.Front()
-				for item != nil {
-					users.SendMessage(item.Value.(uint32), msg.Payload)
-					item = item.Next()
+				err := injectTransmitterUUID(&msg)
+				if err != nil {
+					log.Error("App:%v say:%s", msg.Aid, msg.Payload)
+				} else {
+					item := msg.Uids.Front()
+					for item != nil {
+						users.SendMessage(item.Value.(uint32), msg.Payload)
+						item = item.Next()
+					}
 				}
 			}
 		}
 	}()
+}
+
+func extractReceiverUUID(msg Message) (*uuid.UUID, error)  {
+	var payload UserPayload
+	err := json.Unmarshal(msg.Payload, payload)
+	if err != nil {
+		return nil, err
+	} else {
+		return &payload.Receiver, nil
+	}
+}
+
+func injectTransmitterUUID(msg *AppMessageFrom) error  {
+	var objmap map[string]*json.RawMessage
+	err := json.Unmarshal(msg.Payload, &objmap)
+	if err != nil {
+		return err
+	}
+
+	json_value := json.RawMessage(msg.Aid.String())
+	objmap["Transmitter"] = &json_value
+	payload, err := json.Marshal(objmap);
+	if err != nil {
+		return err
+	}
+
+	msg.Payload = payload
+
+	return nil
 }
